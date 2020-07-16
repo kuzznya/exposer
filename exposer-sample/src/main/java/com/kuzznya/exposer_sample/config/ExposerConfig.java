9705ef221465507f5949ead7ab8791a175dc6b77
kuzznya <ikuz_2002@mail.ru>
2020-07-16 13:34:33 +0300

    feat: Use builder in sample config through bean creation

--- exposer-sample/src/main/java/com/kuzznya/exposer_sample/config/ExposerConfig.java
+++ exposer-sample/src/main/java/com/kuzznya/exposer_sample/config/ExposerConfig.java
@@ -1,15 +1,10 @@
 package com.kuzznya.exposer_sample.config;
 
 import com.github.kuzznya.exposer.core.config.ExposerConfiguration;
-import com.github.kuzznya.exposer.core.model.EndpointProperty;
-import com.github.kuzznya.exposer.core.model.RouteProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @Profile("code")
@@ -17,31 +12,19 @@ public class ExposerConfig {
 
     @Bean
     ExposerConfiguration exposerConfiguration() {
-        return new ExposerConfiguration(
-                List.of(
-                        new RouteProperty(
-                                "/test",
-                                null,
-                                List.of(
-                                        new RouteProperty(
-                                                "/v1",
-                                                "TestService",
-                                                Collections.emptyList(),
-                                                List.of(
-                                                        new EndpointProperty(
-                                                                RequestMethod.GET,
-                                                                null,
-                                                                "getValue",
-                                                                null
-                                                        )
-                                                )
-                                        )
-                                ),
-                                Collections.emptyList()
-                        )
-                ),
-                Collections.emptyList(),
-                "TestService2"
-        );
+        return ExposerConfiguration.builder()
+                .bean("TestService2")
+                .route("/test")
+                    .route("/v1")
+                        .bean("TestService")
+                        .endpoint(RequestMethod.GET, "getValue").and()
+                        .endpoint(RequestMethod.POST, "setValue").register()
+                    .and()
+                    .route("/v2")
+                        .endpoint(RequestMethod.GET, "setValue").bean("TestService").param("value", "?val").register()
+                    .and()
+                    .endpoint(RequestMethod.GET, "joinTwoArgs").param("arg1", "?val").param("arg2", "tst").register()
+                    .add()
+                .configure();
     }
 }
