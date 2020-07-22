090844775c82ad53f4e2361122816266a7b11cc9
kuzznya <ikuz_2002@mail.ru>
2020-07-22 12:12:11 +0300

    feat: Add JSON serialization example in sample project

--- exposer-sample/src/main/java/com/kuzznya/exposer_sample/service/TestService2.java
+++ exposer-sample/src/main/java/com/kuzznya/exposer_sample/service/TestService2.java
@@ -2,10 +2,17 @@ package com.kuzznya.exposer_sample.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TestService2 {
-    public String someFun() {
-        return "Some func";
+    public static class ListSizeResult {
+        public int size;
+        ListSizeResult(int size) {this.size = size;}
+    }
+
+    public ListSizeResult getListSize(List<String> list) {
+        return new ListSizeResult(list.size());
     }
 
     public String joinTwoArgs(String arg1, String arg2) {
