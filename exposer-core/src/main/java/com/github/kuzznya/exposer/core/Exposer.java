e0abf9460828d0da52b9d341a2d2e94a4ee14a2f
kuzznya <ikuz2002@gmail.com>
2020-07-28 11:18:36 +0300

    feat: Unregister mappings before Exposer bean will be destroyed

--- exposer-core/src/main/java/com/github/kuzznya/exposer/core/Exposer.java
+++ exposer-core/src/main/java/com/github/kuzznya/exposer/core/Exposer.java
@@ -6,7 +6,6 @@ import com.github.kuzznya.exposer.core.model.Endpoint;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.DefaultParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -14,7 +13,9 @@ import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -27,6 +28,8 @@ public class Exposer {
     private final ApplicationContext context;
 
     private final ParameterNameDiscoverer paramsDiscoverer = new DefaultParameterNameDiscoverer();
+
+    private final List<RequestMappingInfo> mappings = new ArrayList<>();
 
     public Exposer(RequestMappingHandlerMapping handlerMapping,
                    ExposerConfigurer exposerConfigurer,
@@ -55,20 +58,32 @@ public class Exposer {
             EndpointHandler handler = new EndpointHandler(service, serviceMethod, endpoint.getParamsMapping(),
                     paramsDiscoverer);
 
+            RequestMappingInfo mappingInfo = RequestMappingInfo
+                    .paths(endpoint.getPath())
+                    .methods(endpoint.getHttpMethod())
+                    .params(
+                            endpoint.getParamsMapping() != null ?
+                                    endpoint.getRequestParams().toArray(String[]::new) :
+                                    Optional.ofNullable(paramsDiscoverer.getParameterNames(serviceMethod))
+                                            .orElse(new String[0])
+                    )
+                    .build();
+
             handlerMapping.registerMapping(
-                    RequestMappingInfo
-                            .paths(endpoint.getPath())
-                            .methods(endpoint.getHttpMethod())
-                            .params(
-                                    endpoint.getParamsMapping() != null ?
-                                            endpoint.getRequestParams().toArray(String[]::new) :
-                                            Optional.ofNullable(paramsDiscoverer.getParameterNames(serviceMethod))
-                                                    .orElse(new String[0])
-                            )
-                            .build(),
+                    mappingInfo,
                     handler,
                     EndpointHandler.class.getDeclaredMethod("handle", MultiValueMap.class, Map.class)
             );
+
+            mappings.add(mappingInfo);
+        }
+    }
+
+    @PreDestroy
+    public void unregisterEndpoints() {
+        for (RequestMappingInfo mappingInfo : mappings) {
+            handlerMapping.unregisterMapping(mappingInfo);
+            System.out.println(mappingInfo.getPatternsCondition().getPatterns());
         }
     }
 
